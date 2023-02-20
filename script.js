d5390ed35ebbebf4b72a1421b541660cdc9d8e9e
leoyhk <leoyuen61@gmail.com>
2023-02-20 09:23:39 +0800

    Finished Layout

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Scripts loaded!");
+
+cards = document.getElementById("cards");
+console.log(cards);
+
+input = document.getElementById("number-of-cards");
+console.log(input);
+
+cards.addEventListener("click", (e) => {
+  console.log(e.srcElement);
+});
+
+input.addEventListener("submit", (e) => {
+  console.log(e);
+});
